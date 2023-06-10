4860ccc8b9551bbfa65c121f94f8e007145293bc
MELLI2022 <denisenkoswetlana109@gmail.com>
2023-06-10 22:15:09 +0300

    ДЗ

---
+++ src/main/java/org/example/ElementNotFoundException.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import javax.management.RuntimeErrorException;
+
+public class ElementNotFoundException extends RuntimeErrorException {
+
+
+    public ElementNotFoundException() {
+        super(e, message);
+    }
+}
